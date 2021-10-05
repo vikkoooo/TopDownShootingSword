c1a3ad3b788b5e221c7e12286983d63d71838cd2
vikkoooo <vikkolundberg@gmail.com>
2021-10-05 10:55:38 +0200

    changed unity version

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -7,62 +7,64 @@ using UnityEngine.EventSystems;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // To move the body
-    private float speed = 10f;
-    private Rigidbody2D body;
-    
-    // Other
-    private bool isWalking;
-    
-    private void Start()
-    {
-        body = GetComponent<Rigidbody2D>(); // Initialize body on start
-    }
+	// To move the body
+	private float speed = 10f;
+	private Rigidbody2D body;
 
-    private void Update()
-    {
+	// Other
+	private bool isWalking;
 
-    }
+	private void Start()
+	{
+		body = GetComponent<Rigidbody2D>(); // Initialize body on start
+	}
 
-    private void FixedUpdate()
-    {
-        Move(); // Check for movement. Move functions always in FixedUpdate or we need to use * deltaTime
-    }
-    
-    // Basic movement script along the x and y axis
-    private void Move()
-    {
-        float horizontal = Input.GetAxisRaw("Horizontal");
-        float vertical = Input.GetAxisRaw("Vertical");
-        body.velocity = new Vector2(horizontal * speed, vertical * speed);
-        
-        if (horizontal > 0)
-        {
-            this.transform.rotation = Quaternion.identity;
-            this.transform.Rotate(0, 0, -90);
-            isWalking = true;
-        }
-        // Turn left
-        else if (horizontal < 0)
-        {
-            this.transform.rotation = Quaternion.identity;
-            this.transform.Rotate(0, 0, 90);
-            isWalking = true;
-        }
-        // Up
-        
-        
-        // Down
-        
-        // Standing still
-        //else
-        //{
-            // To reset the wobble when we stop walking, making the wizard stand with both feet on ground
-        //    this.transform.rotation = Quaternion.identity; // .identity means "no rotation"
-        //    isWalking = false;
-        //}  
-        
-    }
+	private void Update()
+	{
+	}
+
+	private void FixedUpdate()
+	{
+		Move(); // Check for movement. Move functions always in FixedUpdate or we need to use * deltaTime
+	}
+
+	// Basic movement script along the x and y axis
+	private void Move()
+	{
+		float horizontal = Input.GetAxisRaw("Horizontal");
+		float vertical = Input.GetAxisRaw("Vertical");
+		body.velocity = new Vector2(horizontal * speed, vertical * speed);
+
+		if (horizontal > 0)
+		{
+			this.transform.rotation = Quaternion.identity;
+			this.transform.Rotate(0, 0, -90);
+			isWalking = true;
+		}
+		// Turn left
+		else if (horizontal < 0)
+		{
+			this.transform.rotation = Quaternion.identity;
+			this.transform.Rotate(0, 0, 90);
+			isWalking = true;
+		}
+		// Up
+		else if (vertical > 0)
+		{
+		}
+
+		// Down
+		else if (vertical < 0)
+		{
+		}
 
 
+		// Standing still
+		//else
+		//{
+		// To reset the wobble when we stop walking, making the wizard stand with both feet on ground
+		//    this.transform.rotation = Quaternion.identity; // .identity means "no rotation"
+		//    isWalking = false;
+		//}  
+	}
 }
