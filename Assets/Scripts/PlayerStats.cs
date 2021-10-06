711d2778a64bebb0370be115db91f1e25feb1d66
vikkoooo <vikkolundberg@gmail.com>
2021-10-06 13:49:56 +0200

    forgot to make the dude die and check for overhealth

--- Assets/Scripts/PlayerStats.cs
+++ Assets/Scripts/PlayerStats.cs
@@ -27,7 +27,21 @@ public class PlayerStats : MonoBehaviour
 
     public void SetHealth(int newHealth)
     {
+        // Updates health to the new value. Can modify it to subtract or add or something if we want
         health = newHealth;
+
+        // In hase of health higher than maximum health possible
+        // Sets the health to maximum number of hearts, in other words, the size of the array.
+        if (health > hearts.Length)
+        {
+            health = hearts.Length;
+        }
+        // In case if player should die, health reaches 0 or below
+        else if (health <= 0)
+        {
+            Debug.Log("YOU DIEDED!");
+            health = 0; // Set to 0 so the update hearts thing below gets to update to all empty
+        }
         
         // Update hearts
         for (int i = 0; i < hearts.Length; i++)
