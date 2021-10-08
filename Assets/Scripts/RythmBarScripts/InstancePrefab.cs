c56d68a5fc6c575bcf72b5a48f4d9b75cc816536
Martin Ohlsson <90261294+MMAAOOHH@users.noreply.github.com>
2021-10-08 11:42:56 +0200

    Script Name Change

---
+++ Assets/Scripts/RythmBarScripts/InstancePrefab.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+namespace RythmBarScripts
+{
+    public class InstancePrefab : MonoBehaviour
+    {
+        public GameObject prefab;
+        public GameObject instanceOn;
+    
+        [Range(0, 16)]
+        public int numberToCreate;
+    
+        private void Start()
+        {
+            InstanceObjects();
+        }
+      
+        public void InstanceObjects()
+        {
+            BoxCollider2D spawnArea = instanceOn.GetComponent<BoxCollider2D>();
+            GameObject instance;
+            float areaX, areaY;
+            Vector2 pos;
+        
+            for (int i = 0; i < numberToCreate; i++)
+            {
+                instance = prefab;
+                areaX = Random.Range(spawnArea.bounds.min.x, spawnArea.bounds.max.x);
+                areaY = Random.Range(spawnArea.bounds.min.y, spawnArea.bounds.max.y);
+                pos = new Vector2(areaX, areaY);
+                Instantiate(prefab, instanceOn.transform);
+            }
+        }
+    }
+}
