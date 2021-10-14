a4e3a8492292afa43a75a05b02a354a99dccb598
Martin Ohlsson <90261294+MMAAOOHH@users.noreply.github.com>
2021-10-14 14:00:14 +0200

    Fixed bug

--- Assets/Scripts/PlayerScripts/PlayerStats.cs
+++ Assets/Scripts/PlayerScripts/PlayerStats.cs
@@ -58,7 +58,7 @@ public class PlayerStats : MonoBehaviour
 
     private void PlayerDied()
     {
-        Destroy(gameObject);
+        // Destroy(gameObject);
         MenuManager.YouDied();
     }
 }
