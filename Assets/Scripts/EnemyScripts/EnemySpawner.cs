c356cae5ccc8efd67eb4f626989febe82eec23a6
Viktor Lundberg <vikkolundberg@gmail.com>
2021-10-14 11:52:53 +0200

    enemy spawner added support for 5 areas

--- Assets/Scripts/EnemyScripts/EnemySpawner.cs
+++ Assets/Scripts/EnemyScripts/EnemySpawner.cs
@@ -7,19 +7,35 @@ using Random = UnityEngine.Random;
 public class EnemySpawner : MonoBehaviour
 {
 	public GameObject[] monsters;
-	private int n_monsters = 10;
+	private int n_monsters_per_area = 2;
 	
     public GameObject area1;
+    public GameObject area2;
+    public GameObject area3;
+    public GameObject area4;
+    public GameObject area5;
     private BoxCollider2D area1_collider;
-
+    private BoxCollider2D area2_collider;
+    private BoxCollider2D area3_collider;
+    private BoxCollider2D area4_collider;
+    private BoxCollider2D area5_collider;
+    
     private void Awake()
     {
 	    area1_collider = area1.GetComponent<BoxCollider2D>();
+	    area2_collider = area2.GetComponent<BoxCollider2D>();
+	    area3_collider = area3.GetComponent<BoxCollider2D>();
+	    area4_collider = area4.GetComponent<BoxCollider2D>();
+	    area5_collider = area5.GetComponent<BoxCollider2D>();
     }
 
     void Start()
     {
-	    CreateMonsters(n_monsters, area1_collider);
+	    CreateMonsters(n_monsters_per_area, area1_collider);
+	    CreateMonsters(n_monsters_per_area, area2_collider);
+	    CreateMonsters(n_monsters_per_area, area3_collider);
+	    CreateMonsters(n_monsters_per_area, area4_collider);
+	    CreateMonsters(n_monsters_per_area, area5_collider);
     }
     
     private void CreateMonsters(int n, BoxCollider2D area)
